1a69f1abe8154c7fb4681d45ac4f1d75eec13bf2
Witold Baryluk <baryluk@smp.if.uj.edu.pl>
2018-12-09 07:48:03 +0100

    Add initial test framework and basic tests for column(N) function.

---
+++ tests/run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+FAIL=0
+PASS=0
+
+ALL=$(pwd)
+for t in $(ls -d */)
+do
+  cd $ALL
+  cd $t
+  if [ ! -f command ]; then
+    echo "$t - Test broken. command file missing."
+    FAIL=$((FAIL+1))
+    continue
+  fi
+  if [ ! -f expected ]; then
+    echo "$t - Test broken. expected file missing."
+    FAIL=$((FAIL+1))
+    continue
+  fi
+  KOLUMNY=../../kolumny sh command 2>&1 | sed -E -e 's|File "([^"]+kolumny)", line [0-9]+, in|File "\1", line xxx, in|' > actual
+  if ! diff -Nu expected actual >/dev/null 2>/dev/null; then
+    echo "$t - Test FAIL. Differences detected:"
+    diff -Nu expected actual
+    echo
+    FAIL=$((FAIL+1))
+  else
+    echo "$t - Test PASS."
+    PASS=$((PASS+1))
+  fi
+done
+
+echo "${PASS} tests passed!"
+echo "${FAIL} tests failed!"
+if [ "x${FAIL}" != "x0" ]; then
+  exit 1
+fi
+
+exit 0
